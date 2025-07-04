b3a27bea6e8888c5de02237afefd569d8f268753
david botero <davids.boteron@gmail.com>
2025-07-04 15:45:29 -0500

    Final changes

--- main.tf
+++ main.tf
@@ -3,55 +3,108 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
+locals {
+  # Naming convention
+  name_prefix        = "${var.client}-${var.app}-${var.environment}"
+  resource_group_name = "${var.client}-${var.app}-${var.environment}"
+  
+  # Common tags
+  common_tags = {
+    client     = var.client
+    app        = var.app
+    environment = var.environment
+    managed_by = "terraform"
+  }
+  
+  # PostgreSQL configuration
+  pg_version         = var.pg_version
+  pg_storage_mb      = var.pg_storage_mb
+  pg_zone            = "1"
+  pg_sku_name        = var.pg_sku_name
+  pg_database_name   = "n8n"
+  pg_port            = "5432"
+  
+  # n8n configuration
+  n8n_port           = "5678"
+  n8n_protocol       = "https"
+  n8n_timezone       = "America/Bogota"
+  n8n_host           = "timia-n8n-dev.redglacier-1a406574.eastus2.azurecontainerapps.io"
+  n8n_webhook_url    = "https://timia-n8n-dev.redglacier-1a406574.eastus2.azurecontainerapps.io"
+  n8n_editor_url     = "https://timia-n8n-dev.redglacier-1a406574.eastus2.azurecontainerapps.io"
+  
+  # Container configuration
+  container_cpu      = var.container_cpu
+  container_memory   = var.container_memory
+  container_image    = var.container_image
+  min_replicas       = var.min_replicas
+  max_replicas       = var.max_replicas
+  
+  # SSL configuration
+  ssl_reject_unauthorized = "false"
+  ssl_mode                = "require"
+}
+
+# Crear el Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = local.resource_group_name
+  location = var.location
+
+  tags = local.common_tags
+}
+
 resource "azurerm_postgresql_flexible_server" "n8n_db" {
-  name                   = var.pg_server_name
-  location               = var.location
-  resource_group_name    = var.resource_group_name
+  name                   = local.name_prefix
+  location               = azurerm_resource_group.main.location
+  resource_group_name    = azurerm_resource_group.main.name
   administrator_login    = var.pg_admin_user
   administrator_password = var.pg_admin_pass
-  version                = "14"
-  storage_mb             = 32768
-  zone                   = "1"
+  version                = local.pg_version
+  storage_mb             = local.pg_storage_mb
+  zone                   = local.pg_zone
 
-  sku_name = "B_Standard_B1ms"
+  sku_name = local.pg_sku_name
 
   authentication {
     password_auth_enabled = true
   }
+
+  tags = local.common_tags
 }
 
 resource "azurerm_postgresql_flexible_server_database" "n8n_database" {
-  name      = "n8n"
+  name      = local.pg_database_name
   server_id = azurerm_postgresql_flexible_server.n8n_db.id
   charset   = "UTF8"
   collation = "en_US.utf8"
 }
 
 resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure_services" {
-  name             = "AllowAzureServices"
+  name             = local.name_prefix
   server_id        = azurerm_postgresql_flexible_server.n8n_db.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
 }
 
 resource "azurerm_container_app_environment" "n8n_env" {
-  name                = "n8n-env"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  name                = local.name_prefix
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = local.common_tags
 }
 
 resource "azurerm_container_app" "n8n_app" {
-  name                         = "n8n-app"
+  name                         = local.name_prefix
   container_app_environment_id = azurerm_container_app_environment.n8n_env.id
-  resource_group_name          = var.resource_group_name
+  resource_group_name          = azurerm_resource_group.main.name
   revision_mode                = "Single"
 
   template {
     container {
       name   = "n8n"
-      image  = "n8nio/n8n:latest"
-      cpu    = 0.5
-      memory = "1.0Gi"
+      image  = local.container_image
+      cpu    = local.container_cpu
+      memory = local.container_memory
 
       env {
         name  = "DB_TYPE"
@@ -63,11 +116,11 @@ resource "azurerm_container_app" "n8n_app" {
       }
       env {
         name  = "DB_POSTGRESDB_PORT"
-        value = "5432"
+        value = local.pg_port
       }
       env {
         name  = "DB_POSTGRESDB_DATABASE"
-        value = "n8n"
+        value = local.pg_database_name
       }
       env {
         name  = "DB_POSTGRESDB_USER"
@@ -83,49 +136,51 @@ resource "azurerm_container_app" "n8n_app" {
       }
       env {
         name  = "N8N_HOST"
-        value = var.n8n_host
+        value = local.n8n_host
       }
       env {
         name  = "N8N_PORT"
-        value = var.n8n_port
+        value = local.n8n_port
       }
       env {
         name  = "DB_POSTGRESDB_SSL_REJECT_UNAUTHORIZED"
-        value = "false"
+        value = local.ssl_reject_unauthorized
       }
       env {
         name  = "DB_POSTGRESDB_SSL_MODE"
-        value = "require"
+        value = local.ssl_mode
       }
       env {
         name  = "N8N_PROTOCOL"
-        value = "https"
+        value = local.n8n_protocol
       }
       env {
         name  = "WEBHOOK_URL"
-        value = "https://${var.n8n_host}"
+        value = local.n8n_webhook_url
       }
       env {
         name  = "N8N_EDITORBASE_URL"
-        value = "https://${var.n8n_host}"
+        value = local.n8n_editor_url
       }
       env {
         name  = "TZ"
-        value = "America/Bogota"
+        value = local.n8n_timezone
       }
     }
 
-    min_replicas = 1
-    max_replicas = 1
+    min_replicas = local.min_replicas
+    max_replicas = local.max_replicas
   }
 
   ingress {
     external_enabled = true
-    target_port      = 5678
+    target_port      = local.n8n_port
 
     traffic_weight {
       latest_revision = true
       percentage      = 100
     }
   }
+
+  tags = local.common_tags
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,52 @@
-variable "resource_group_name" {
-  default = "timia-learning-dev_group"
+variable "client" {
+  description = "Nombre del cliente"
+  default     = "timia"
+}
+
+variable "app" {
+  description = "Nombre de la aplicación"
+  default     = "n8n"
+}
+
+variable "environment" {
+  description = "Ambiente de despliegue"
+  default     = "dev"
 }
 
 variable "location" {
-  default = "eastus2"
-}
-
-variable "pg_server_name" {
-  default = "n8npgflex"
+  description = "Región de Azure donde se desplegarán los recursos"
+  default     = "eastus2"
 }
 
 variable "pg_admin_user" {
-  default = "n8nadmin"
+  description = "Usuario administrador de PostgreSQL"
+  default     = "n8nadmin"
+}
+
+variable "pg_admin_pass" {
+  description = "Contraseña del administrador de PostgreSQL"
+  sensitive   = true
+}
+
+variable "n8n_encryption_key" {
+  description = "Clave de encriptación para n8n"
+  sensitive   = true
 }
 
 variable "subscription_id" {}
 
-variable "pg_admin_pass" {}
+variable "pg_version" {}
 
-variable "n8n_encryption_key" {}
+variable "pg_storage_mb" {}
 
-variable "n8n_host" {}
+variable "pg_sku_name" {}
 
-variable "n8n_port" {}
+variable "container_cpu" {}
+
+variable "container_memory" {}
+
+variable "container_image" {}
+
+variable "min_replicas" {}
+
+variable "max_replicas" {}
